8f1c103e054f98953817fdf5ca5774d34bbd6034
slouchart <sebastien.louchart.itp@gmail.com>
2019-10-01 16:28:28 +0200

    fix test order

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['test_all']
 
-from unittest import TestSuite
+from unittest import TestSuite, defaultTestLoader
 
 import tests.test_primefield as t1
 import tests.test_polynomial as t2
@@ -18,9 +18,10 @@ NB_TESTS = 10
 
 def test_all():
     suite = TestSuite()
+    loader = defaultTestLoader
     for i in range (1, NB_TESTS+1):
         alias = f't{i}'
         module = globals()[alias]
-        suite.addTest(module)
+        suite.addTest(loader.loadTestsFromModule(module))
 
     return suite
