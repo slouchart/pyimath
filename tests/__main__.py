62e33af4dc8abfa17cb719873b9181a8e7767b6c
Sébastien LOUCHART <sebastien.louchart@ext.recia.fr>
2019-12-24 16:18:45 +0100

    implement a better test suite

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,27 +1,31 @@
 __all__ = ['load_tests']
 
-from unittest import TestSuite, defaultTestLoader
+from importlib import import_module
+from unittest import TestSuite
 from unittest import main as run_tests
 
-import tests.test_primefield as t1
-import tests.test_polynomial as t2
-import tests.test_finitefield as t3
-import tests.test_polyparse as t4
-import tests.test_transtype as t5
-import tests.test_pfpoly as t6
-import tests.test_ffpoly as t7
-import tests.test_factorize as t8
-import tests.test_ff_factorize as t9
-import tests.test_gaussint as t10
+
 
 NB_TESTS = 10
+
+TEST_CASES = [
+    'primefield',
+    'polynomial',
+    'finitefield',
+    'polyparse',
+    'transtype',
+    'pfpoly',
+    'ffpoly',
+    'factorize',
+    'ff_factorize',
+    'gaussint',
+]
 
 
 def load_tests(loader, standard_tests, pattern):
     suite = TestSuite()
-    for i in range(1, NB_TESTS+1):
-        alias = f't{i}'
-        module = globals()[alias]
+    for module_stem in TEST_CASES:
+        module = import_module(f'tests.test_{module_stem}')
         suite.addTest(loader.loadTestsFromModule(module))
 
     return suite
